b5921b22c45ace491455faed3982644739557f7c
jaewook2400 <101032947+jaewook2400@users.noreply.github.com>
2025-07-19 20:22:30 +0900

    update register_page.dart

--- login_frontend/lib/main.dart
+++ login_frontend/lib/main.dart
@@ -4,11 +4,7 @@ import 'package:login_frontend/register_page.dart';
 void main(){
   runApp(
     MaterialApp(
-      home: Column(
-        children: [
-          RegisterPage(),
-        ],
-      ),
-    )
+      home: RegisterPage(),
+    ),
   );
 }
--- login_frontend/lib/register_page.dart
+++ login_frontend/lib/register_page.dart
@@ -11,7 +11,13 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      
+      backgroundColor: Color(0xFF335CB0),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Hello")
+        ],
+      ),
       // TextFields, 버튼 등 포함ㅇㅇㅇㅇ
     );
   }
